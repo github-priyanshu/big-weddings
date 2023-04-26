faa856ff5cec91ff10bcfc52d9a48a51d3756b07
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-04-26 14:15:23 +0530

    Add files via upload

---
+++ js/makeFooter.js
@@ -0,0 +1,61 @@
+var footHtml=`
+<div class="lineMargin" m="20px"></div>
+<div class="footer flex c w100p">
+  <div class="knowUs">
+    <h2>About <b>Big Weddings</b></h2>
+    <div class="lineMargin" m='10px'></div>
+    <p>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Corporis minus, rem atque harum recusandae? Molestiae, quasi eligendi, omnis nobis eum eveniet dicta sit libero veritatis voluptatem facilis nulla incidunt. Optio, error? Iusto sed accusamus iure itaque culpa minima placeat consequuntur!</p>
+  </div>
+  <div class="downSec flex w100p">
+
+    <div class="contact">
+      <h2>Contact Us <span class="material-symbols-outlined">contact_support</span></h2>
+
+      <div class="contactMain">
+        <a target="__blank" href="tel:+91 803 103 0003" class="conItem flex">
+          <span class="material-symbols-outlined">call</span>
+          <span>+91 803 103 0003</span>
+        </a>
+        <a target="__blank" href="mailto:help@bigweddings.net" class="conItem flex">
+          <span class="material-symbols-outlined">mail</span>
+          <span>help@bigweddings.net</span>
+        </a>
+      </div>
+
+    </div>
+
+    <div class="follow">
+      <h2>Follow Us <span class="material-symbols-outlined">link</span></h2>
+
+      <div class="followMain">
+        <a target="__blank" href="https://facebook.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-facebook"></ion-icon>
+          facebook
+        </a>
+        <a target="__blank" href="https://twitter.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-twitter"></ion-icon>
+          twitter
+        </a>
+        <a target="__blank" href="https://linkedin.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-linkedin"></ion-icon>
+          linkedin
+        </a>
+        <a target="__blank" href="https://pinterest.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-pinterest"></ion-icon>
+          pinterest
+        </a>
+        <a target="__blank" href="https://instagram.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-instagram"></ion-icon>
+          instagram
+        </a>
+        <a target="__blank" href="https://youtube.com/bigweddings" class="followItem flex ">
+          <ion-icon name="logo-youtube"></ion-icon>
+          youtube
+        </a>
+      </div>
+
+    </div>
+
+  </div>
+</div>`;
+document.body.insertAdjacentHTML("beforeend",footHtml);
--- js/serviceMaker.js
+++ js/serviceMaker.js
@@ -1,30 +1,69 @@
+var elemAry=[]
+var scrollPanel=opp(".scrollPanel");
+log(scrollPanel);
 
+scrollPanel.forEach(val=>{
+	resetScroller(val);
+	var doing=true,
+	defaultScroll=val.scrollLeft;
+	var n=0;
+	val.onscroll=(e)=>{
+		var x=setTimeout(()=>{
+			doing=true;
+			resetScroller(val);
+		},250);
 
-var htmlServe="";
-var itN=0;
-for(var i=0; i< menuItem.length; i+=4){
-	log('c')
-	htmlServe+=`<div class="serve4 flex">`
-	for(var j=i; j<i+4; j++){
-		htmlServe+=`
-      <div class="serve">
-        <img src="img/services/${menuItem[j][1]}.png" alt="${menuItem[j][1]} service">
-        <p class="texCen w100p">${menuItem[j][1]}</p>
-      </div>
-`
-	}	
-	htmlServe+=`</div>`;
+		if(doing){
+			clearTimeout(x);
+			var sign=Math.sign(defaultScroll - val.scrollLeft);
+			if(sign)
+				changeActive(sign,val);
+		}
+
+		doing=false;
+	}
+})
+function resetScroller(elem){
+	doing=false;
+	setTimeout(()=>{doing=true},250);
+	elem.scrollTo(elem.offsetWidth/2,0);	
 }
 
-var serveHtml=`
-<div class="lineMargin" m="30px"></div>
-<div class="services flex c w100p">
-  <h1>What we do..!! <span class="material-symbols-outlined">settings_suggest</span></h1>
+function changeActive(sign,val){
+	val.classList.add("thisisscrolled");
+	
+	var main=op(".thisisscrolled .scr.scrMain"),
+	left=op(".thisisscrolled .scr.scrLeft"),
+	right=op(".thisisscrolled .scr.scrRight");
 
-  <div class="lineMargin" m="10px"></div>
+	val.classList.remove("thisisscrolled");
 
-  <div class="serveBox flex">${htmlServe}</div>
-</div>
-`
-document.body.insertAdjacentHTML("beforeend",serveHtml);
-resetFormat();
+
+	if(sign<0){
+		main.style.left='-66%';
+		right.style.left='10%';
+		left.style.left='-100%';
+
+		main.classList.remove('scrMain');
+		main.classList.add('scrLeft');
+
+		right.classList.remove("scrRight");
+		right.classList.add("scrMain");
+
+		left.classList.remove("scrLeft");
+		left.classList.add("noshow");
+	}else{
+		main.style.left='86%';
+		right.style.left='100%';
+		left.style.left='10%';
+
+		main.classList.remove('scrMain');
+		main.classList.add('scrRight');
+
+		right.classList.remove("scrRight");
+		right.classList.add("noshow");
+
+		left.classList.remove("scrLeft");
+		left.classList.add("scrMain");
+	}
+}
